c42615ca00b968c132959649752f453447a0f584
Michael <punky@linux-dell-9010.int.bit2atom.de>
2020-10-11 15:38:26 +0200

    Add an Config File

--- tunnel.sh
+++ tunnel.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-remote_host="moomers.org"
-remote_user="pitunnel"
-remote_port=2222
+config() {
+    val=$(grep -E "^$1=" pitunnel.conf 2>/dev/null || echo "$1=__DEFAULT__" | head -n 1 | cut -d '=' -f 2-)
+
+    if [[ $val == __DEFAULT__ ]]
+    then
+        case $1 in
+            remote_host)
+                echo -n "localhost"
+                ;;
+            remote_user)
+                echo -n "mn"
+                ;;
+            remote_port)
+                echo -n "8000"
+                ;;
+        esac
+    else
+        echo -n $val
+    fi
+}
+
+remote_host=$(config remote_host)
+remote_user=$(config remote_user)
+remote_port=$(config remote_user)
 
 # some helpful debug info
 echo "Connecting to '${remote_host}' as user '${remote_user}'..."
